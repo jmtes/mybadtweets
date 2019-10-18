3c68451c3fa5d7431e2d359cd237f2607760963f
jmtes <juliennetesoro@gmail.com>
2019-10-18 12:29:55 -0700

    Init passport and cookie session

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const passportSetup = require('./config/passport-setup');
 const session = require('express-session');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -16,6 +18,15 @@ app.use(session({
 
 // Set up view engine
 app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [keys.session.cookieKey]
+}));
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect to mongodb
 mongoose.connect(keys.mongodb.dbURI, () => {
