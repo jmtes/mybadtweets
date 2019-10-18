9448dc2da76b57a1090255bd10f1494400289b84
jmtes <juliennetesoro@gmail.com>
2019-10-18 12:30:10 -0700

    Add key

--- config/keys.js
+++ config/keys.js
@@ -5,5 +5,8 @@ module.exports = {
   },
   mongodb: {
     dbURI: `mongodb+srv://${process.env.MONGODBUSER}:${process.env.MONGODBPWD}@hosanna-ikelv.mongodb.net/test?retryWrites=true&w=majority`
+  },
+  session: {
+    cookieKey: 'star'
   }
 };
