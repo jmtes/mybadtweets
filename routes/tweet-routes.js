501968f7de3009be3690ecc74a003e50feb8de60
jmtes <juliennetesoro@gmail.com>
2019-10-19 13:00:29 -0700

    Test tweet retrieval

--- routes/tweet-routes.js
+++ routes/tweet-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const Twit = require('twit');
+const keys = require('../config/keys');
 
 const authCheck = (req, res, next) => {
   if (!req.user) {
@@ -11,8 +13,33 @@ const authCheck = (req, res, next) => {
 }
 
 router.get('/', authCheck, (req, res) => {
+  const params = {
+    screen_name: req.user.username,
+    count: 200,
+    trim_user: true,
+    exclude_replies: true,
+    include_rts: false
+  }
+
+  const client = new Twit({
+    consumer_key: keys.twitter.consumerKey,
+    consumer_secret: keys.twitter.consumerSecret,
+    access_token: req.user.accessToken,
+    access_token_secret: req.user.accessTokenSecret
+  });
+
+  function getTweets() {
+    client.get('statuses/user_timeline', params, (err, data, response) => {
+      console.log(data);
+      return data;
+    });
+  }
+
+  const tweetArray = getTweets();
+  console.log(tweetArray);
   res.render('tweets', {
-    user: req.user
+    user: req.user,
+    tweets: tweetArray
   });
 });
 
