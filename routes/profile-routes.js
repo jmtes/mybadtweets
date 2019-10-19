4e25d981274e46ddaae67a70191a674938b5cc5a
jmtes <juliennetesoro@gmail.com>
2019-10-19 10:15:13 -0700

    Fix typo

--- routes/profile-routes.js
+++ routes/profile-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.get('/'. (req, res) => {
+router.get('/', (req, res) => {
   res.send('You are logged in, this is your profile: ' + req.user.username);
 })
 
