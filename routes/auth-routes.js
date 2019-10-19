4ce563bfbad91961e1fe52a6a89806a05b842775
jmtes <juliennetesoro@gmail.com>
2019-10-19 10:14:32 -0700

    Add profile routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -18,7 +18,8 @@ router.get('/twitter', passport.authenticate('twitter'));
 
 // Callback route for Twitter to redirect to
 router.get('/twitter/redirect', passport.authenticate('twitter'), (req, res) => {
-  res.send(req.user);
+  // res.send(req.user);
+  res.redirect('/profile');
 })
 
 module.exports = router;
