5ebbfb777b167626d7fd857163bdf777762acc45
jmtes <juliennetesoro@gmail.com>
2019-10-20 11:33:13 -0700

    Test passed in data

--- static/scripts/delete.js
+++ static/scripts/delete.js
@@ -3,11 +3,12 @@ const deleteButtons = document.querySelectorAll('button.delete');
 deleteButtons.forEach(button => {
   button.addEventListener('click', () => {
     console.log('button pressed. tweet id is ' + button.dataset.tweetid);
+    data = { tweetid: button.dataset.tweetid };
+    console.log('data is: ', data);
+
     fetch('/api/delete', {
       method: 'POST',
-      body: JSON.stringify({
-        tweetid: button.dataset.tweetid
-      }),
+      body: JSON.stringify(data),
       headers: {
         'Content-type': 'application/json'
       }
