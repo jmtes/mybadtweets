1585971813d781068d33d560c24b9e2909052ad7
jmtes <juliennetesoro@gmail.com>
2019-10-20 11:07:08 -0700

    Add request params

--- static/scripts/delete.js
+++ static/scripts/delete.js
@@ -3,7 +3,12 @@ const deleteButtons = document.querySelectorAll('button.delete');
 deleteButtons.forEach(button => {
   button.addEventListener('click', () => {
     console.log('button pressed. tweet id is ' + button.dataset.tweetid);
-    const response = await fetch('/api/delete');
+    const response = await fetch('/api/delete', {
+      method: 'GET',
+      body: JSON.stringify({
+        tweetid: button.dataset.tweetid
+      })
+    });
     const responseJSON = await response.json();
     console.log(JSON.stringify(myJSON));
   })
