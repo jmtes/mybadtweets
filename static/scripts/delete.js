f72ccbbe8b8677107fe039542f498cd03a4cc8a7
jmtes <juliennetesoro@gmail.com>
2019-10-20 12:31:39 -0700

    Fix styling

--- static/scripts/delete.js
+++ static/scripts/delete.js
@@ -4,17 +4,14 @@ deleteButtons.forEach(button => {
   button.addEventListener('click', () => {
     console.log('button pressed. tweet id is ' + button.dataset.tweetid);
     data = { tweetid: button.dataset.tweetid };
-    console.log('data is: ', data);
 
     fetch('/api/delete', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
-        'Content-type': 'application/json'
+        'Content-Type': 'application/json'
       }
     }).then(response => {
-      console.log('inside callback');
-      console.log(response);
       const responseJSON = response.json().then(data => {
         console.log(data);
       });
