f4d873653ffdb96355ffe5be7fbc3e5165744a21
jmtes <juliennetesoro@gmail.com>
2019-10-20 13:58:42 -0700

    Add script and api route for retweeting

---
+++ static/scripts/retweet.js
@@ -0,0 +1,24 @@
+const retweetButtons = document.querySelectorAll('button.retweet');
+
+retweetButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    console.log('button pressed. tweet id is ' + button.parentElement.dataset.tweetid);
+    data = { tweetid: button.parentElement.dataset.tweetid };
+
+    fetch('/api/retweet', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => {
+      const responseJSON = response.json().then(data => {
+        if (data.status === 'OK') {
+          button.parentElement.remove();
+        } else {
+          console.log('try again lol');
+        }
+      });
+    });
+  });
+});
