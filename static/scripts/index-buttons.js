f3ff35832107c74c2f567c75f0f93ea997049402
jmtes <juliennetesoro@gmail.com>
2019-10-22 15:13:10 -0700

    Move function to separate script

--- static/scripts/index-buttons.js
+++ static/scripts/index-buttons.js
@@ -10,14 +10,14 @@ let rainbow;
 //   }, 100);
 // });
 
-document.addEventListener('click', event => {
-  if (event.target.classList.contains('button')) {
-    event.target.style.boxShadow = '0px 0px';
-    setTimeout(() => {
-      event.target.style.boxShadow = '-1px -1px #000';
-    }, 100);
-  }
-});
+// document.addEventListener('click', event => {
+//   if (event.target.classList.contains('button')) {
+//     event.target.style.boxShadow = '0px 0px';
+//     setTimeout(() => {
+//       event.target.style.boxShadow = '-1px -1px #000';
+//     }, 100);
+//   }
+// });
 
 signIn.addEventListener('mouseover', () => {
   // document.querySelector('body').style.backgroundImage = 'url("static/images/fireworks.gif")';
