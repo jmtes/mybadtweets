5a1529f7da38fa0d410b961b818ab299078080eb
jmtes <juliennetesoro@gmail.com>
2019-10-26 16:08:03 -0700

    Specify full URL in strategy

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -18,7 +18,7 @@ passport.use(
     // Options for the strategy
     consumerKey: keys.twitter.consumerKey,
     consumerSecret: keys.twitter.consumerSecret,
-    callbackURL: '/auth/redirect'
+    callbackURL: 'http://mybadtweets.herokuapp.com/auth/redirect'
   }, (token, tokenSecret, profile, done) => {
     // Check if user already exists in db
     User.findOne({twitterID: profile.id}).then((currentUser) => {
