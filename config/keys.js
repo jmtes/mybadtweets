2efb7f08f81aa5164df5798f92186de9437a976b
jmtes <juliennetesoro@gmail.com>
2019-10-26 22:47:37 -0700

    Modify dbURI value

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
   twitter: {
     consumerKey: process.env.CONSUMERKEY,
-    consumerSecret: process.env.CONSUMERSECRET,
+    consumerSecret: process.env.CONSUMERSECRET
   },
   mongodb: {
-    dbURI: process.env.DBURI
+    dbURI: 'mongodb+srv://' + process.env.DBUSER + ':' + process.env.DBPWD + '@hosanna-ikelv.mongodb.net/test?retryWrites=true&w=majority'
   },
   session: {
     cookieKey: 'star'
