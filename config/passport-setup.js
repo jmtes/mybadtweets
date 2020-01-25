5e6a588e0a751faf8d41b5f141fb78030e899ab2
jmtes <juliennetesoro@gmail.com>
2020-01-24 16:28:46 -0800

    Remove old code

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -13,37 +13,6 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-// passport.use(
-//   new Strategy({
-//     // Options for the strategy
-//     consumerKey: keys.twitter.consumerKey,
-//     consumerSecret: keys.twitter.consumerSecret,
-//     callbackURL: '/auth/redirect'
-//   }, (token, tokenSecret, profile, done) => {
-//     // Check if user already exists in db
-//     User.findOne({ twitterID: profile.id }).then((currentUser) => {
-//       if (currentUser) {
-//         // User exists
-//         currentUser.accessToken = token;
-//         currentUser.accessTokenSecret = tokenSecret;
-//         currentUser.save().then((modifiedUser) => {
-//           done(null, modifiedUser);
-//         });
-//       } else {
-//         new User({
-//           username: profile.username,
-//           twitterID: profile.id,
-//           accessToken: token,
-//           accessTokenSecret: tokenSecret
-//         }).save().then((newUser) => {
-//           console.log('new user created' + newUser);
-//           done(null, newUser);
-//         });
-//       }
-//     });
-//   })
-// );
-
 passport.use(new Strategy({
   consumerKey: keys.twitter.consumerKey,
   consumerSecret: keys.twitter.consumerSecret,
