27b9d621848440592f7029c36287979c65533773
jmtes <juliennetesoro@gmail.com>
2020-01-28 11:57:39 -0800

    Add getNextTweet function

--- static/scripts/slideshow.js
+++ static/scripts/slideshow.js
@@ -1,4 +1,13 @@
 // Tweet generator
 module.exports = class Slides {
+  // tweets param should be array of tweet objects
+  constructor (tweets) {
+    this.tweets = tweets;
+    this.currentIndex = -1;
+  }
 
+  getNextTweet () {
+    this.currentIndex = (this.currentIndex + 1) % this.tweets.length;
+    return this.tweets[this.currentIndex];
+  }
 };
