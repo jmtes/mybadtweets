e362b519245e33f7a20a089d0230875cb9abfa04
jmtes <juliennetesoro@gmail.com>
2020-02-04 11:42:21 -0800

    Unnest functions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,19 +4,52 @@ const keys = require('../config/keys');
 const bodyParser = require('body-parser').json();
 const math = require('mathjs');
 
+// Check if user is logged in
 const authCheck = (req, res, next) => {
   if (!req.user) {
-    // If user is not logged in
+    // User is not logged in, redirect to auth page
     res.redirect('/auth');
   } else {
-    // If user is logged in
+    // User is logged in, proceed
     next();
   }
 };
 
+// Create new Twit instance from user info.
+function newTwit (req) {
+  return new Twit({
+    consumer_key: keys.twitter.consumerKey,
+    consumer_secret: keys.twitter.consumerSecret,
+    access_token: req.user.accessToken,
+    access_token_secret: req.user.accessTokenSecret
+  });
+}
+
+// Determine how few likes a tweet must have to be considered "bad".
+// @param tweets - array of tweet objects
+function calcLikeThreshold (tweets) {
+  const tweetLikes = [];
+
+  // Add each tweet's like count to list
+  tweets.forEach(function (tweet) {
+    tweetLikes.push(tweet.favorite_count);
+  });
+
+  // From list, calculate average amount of likes and standard deviation.
+  const avg = parseInt(math.mean(tweetLikes));
+  let stdDev = parseInt(math.std(tweetLikes));
+
+  // If standard deviation is greater than average, take some mathematical
+  // discretion to ensure like threshold comes out positive.
+  while (stdDev > avg) {
+    stdDev = parseInt(stdDev * 0.8);
+  }
+
+  // Return difference between average and standard deviation.
+  return (avg - stdDev);
+}
+
 router.get('/fetch', authCheck, (req, res) => {
-  console.log('inside fetch route');
-  console.log(`fetching ${req.user.username}'s tweets`);
   let tweetArray = [];
   let i = 16;
 
@@ -29,28 +62,7 @@ router.get('/fetch', authCheck, (req, res) => {
     tweet_mode: 'extended'
   };
 
-  const client = new Twit({
-    consumer_key: keys.twitter.consumerKey,
-    consumer_secret: keys.twitter.consumerSecret,
-    access_token: req.user.accessToken,
-    access_token_secret: req.user.accessTokenSecret
-  });
-
-  function calcLikeThreshold () {
-    const tweetLikes = [];
-
-    tweetArray.forEach(function (tweet) {
-      tweetLikes.push(tweet.favorite_count);
-    });
-
-    const avg = parseInt(math.mean(tweetLikes));
-    let stdDev = parseInt(math.std(tweetLikes));
-    while (stdDev > avg) {
-      stdDev = parseInt(stdDev * 0.8);
-    }
-
-    return (avg - stdDev);
-  }
+  const client = newTwit(req);
 
   client.get('statuses/user_timeline', params, function makeTweetList (_err, data, response) {
     tweetArray = tweetArray.concat(data);
@@ -59,7 +71,7 @@ router.get('/fetch', authCheck, (req, res) => {
     if (i) {
       client.get('statuses/user_timeline', params, makeTweetList);
     } else {
-      const likeThreshold = calcLikeThreshold();
+      const likeThreshold = calcLikeThreshold(tweetArray);
       let badTweets;
       if (likeThreshold) {
         badTweets = tweetArray.filter(tweet => tweet.favorite_count < likeThreshold);
@@ -85,12 +97,7 @@ router.post('/delete', authCheck, bodyParser, (req, res) => {
   const endpoint = 'statuses/destroy/' + req.body.tweetid;
   console.log(endpoint);
 
-  const client = new Twit({
-    consumer_key: keys.twitter.consumerKey,
-    consumer_secret: keys.twitter.consumerSecret,
-    access_token: req.user.accessToken,
-    access_token_secret: req.user.accessTokenSecret
-  });
+  const client = newTwit(req);
 
   client.post(endpoint, (err, data, response) => {
     console.log('inside delete callback');
@@ -123,12 +130,7 @@ router.post('/retweet', authCheck, bodyParser, (req, res) => {
   const endpoint = 'statuses/retweet/' + req.body.tweetid;
   console.log('endpoint is ' + endpoint);
 
-  const client = new Twit({
-    consumer_key: keys.twitter.consumerKey,
-    consumer_secret: keys.twitter.consumerSecret,
-    access_token: req.user.accessToken,
-    access_token_secret: req.user.accessTokenSecret
-  });
+  const client = newTwit(req);
 
   client.post(endpoint, (err, data, response) => {
     console.log('inside delete callback');
