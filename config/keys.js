5cec672026aee3d5d9789ef2d1f398cdeeb26600
jmtes <juliennetesoro@gmail.com>
2020-02-04 10:54:58 -0800

    Add key

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,14 @@
+/**
+ * This module exports an object containing the keys used to configure the app.
+ * Please refer to the README for instructions on setting them up if you haven't
+ * already.
+ */
+
 module.exports = {
   twitter: {
     consumerKey: process.env.CONSUMERKEY,
-    consumerSecret: process.env.CONSUMERSECRET
+    consumerSecret: process.env.CONSUMERSECRET,
+    callbackURL: process.env.CALLBACKURL
   },
   mongodb: {
     dbURI: process.env.DBURI
