4219301ec1729e9c5fe0f08b459909c502780a1f
jmtes <juliennetesoro@gmail.com>
2020-02-04 10:05:34 -0800

    Change dbURI value

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,7 @@ module.exports = {
     consumerSecret: process.env.CONSUMERSECRET
   },
   mongodb: {
-    dbURI: 'mongodb+srv://' + process.env.DBUSER + ':' + process.env.DBPWD + '@hosanna-ikelv.mongodb.net/test?retryWrites=true&w=majority'
+    dbURI: process.env.DBURI
   },
   session: {
     secret: process.env.SESSIONKEY
