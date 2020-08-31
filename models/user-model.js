4693d36d4dbd267f8eb5fa4227b320c4022f3cf9
jmtes <juliennetesoro@gmail.com>
2020-08-31 14:12:34 -0700

    Add createdAt field and require existing fields

--- models/user-model.js
+++ models/user-model.js
@@ -2,13 +2,32 @@
 
 const mongoose = require('mongoose');
 
+mongoose.set('useCreateIndex', true);
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  username: String,
-  twitterID: String,
-  accessToken: String,
-  accessTokenSecret: String
+  username: {
+    type: String,
+    required: true
+  },
+  twitterID: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    required: true
+  },
+  accessTokenSecret: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+    expires: 86400
+  }
 });
 
 const User = mongoose.model('user', userSchema);
